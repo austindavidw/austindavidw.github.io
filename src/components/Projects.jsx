8ed573743212124a7d8d7e096778a3cb1366c024
Austin Wright <austindavidw@yahoo.com>
2023-05-19 00:30:10 -0500

    added fourth project

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import simonsaysgame from "../assets/simonsaysgame.png";
 import keeperapp from "../assets/keeperapp.png";
 import newslettersignup from "../assets/newslettersignup.png";
+import blogwebsite from "../assets/blogwebsite.png";
 
 const Projects = () => {
   return (
@@ -18,7 +19,7 @@ const Projects = () => {
         {/* First Project */}
 
         <div class="row allCards">
-          <div class="col-lg-4 col-md-12 mb-4">
+          <div class="col-lg-6 col-md-6 mb-4">
             <div class="card rounded-5 h-100">
               <div
                 class="bg-image hover-overlay ripple"
@@ -72,7 +73,7 @@ const Projects = () => {
 
           {/* Second Project */}
 
-          <div class="col-lg-4 col-md-6 mb-4">
+          <div class="col-lg-6 col-md-6 mb-4">
             <div class="card rounded-5 h-100">
               <div
                 class="bg-image hover-overlay ripple"
@@ -125,7 +126,7 @@ const Projects = () => {
 
           {/* Third Project */}
 
-          <div class="col-lg-4 col-md-6 mb-4">
+          <div class="col-lg-6 col-md-6 mb-4">
             <div class="card rounded-5 h-100">
               <div
                 class="bg-image hover-overlay ripple"
@@ -167,6 +168,60 @@ const Projects = () => {
                   </a>
                   <a
                     href="https://whispering-ravine-04277.herokuapp.com/"
+                    class="btn btn-dark mx-2"
+                    role={"button"}
+                  >
+                    Website
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          {/* Fourth Project */}
+
+          <div class="col-lg-6 col-md-6 mb-4">
+            <div class="card rounded-5 h-100">
+              <div
+                class="bg-image hover-overlay ripple"
+                data-mdb-ripple-color="light"
+              >
+                <img
+                  src={blogwebsite}
+                  class="img-fluid rounded-top-5"
+                  alt="newslettersignuppage"
+                />
+                <a href="#!">
+                  <div
+                    class="mask"
+                    style={{ backgroundColor: "rgba(251, 251, 251, 0.15)" }}
+                  ></div>
+                </a>
+              </div>
+              <div class="card-body bg-secondary bg-opacity-25 rounded-bottom-5">
+                <h5
+                  class="card-title text-muted"
+                  style={{ fontFamily: "Lato, sans-serif" }}
+                >
+                  Demo Blog Website
+                </h5>
+                <p
+                  class="card-text text-muted"
+                  style={{ fontFamily: "Lato, sans-serif" }}
+                >
+                  Heroku deployed website using Node.js, Express and MongoDB for
+                  the database!
+                </p>
+                <div className="d-grid gap-2 d-lg-block">
+                  <a
+                    href="https://github.com/austindavidw/blog-db-website"
+                    class="btn btn-outline-dark mx-2"
+                    role={"button"}
+                  >
+                    Code
+                  </a>
+                  <a
+                    href="https://rocky-plains-89731.herokuapp.com/"
                     class="btn btn-dark mx-2"
                     role={"button"}
                   >
